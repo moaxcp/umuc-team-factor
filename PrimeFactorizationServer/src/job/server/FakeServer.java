cf1b1ecac851f5d74595612be06ca3ea043cb2e4
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-11-14 20:29:25 +0000

    Created a FakeServer for testing the client. It will issue Jobs that count to 30.

---
+++ PrimeFactorizationServer/src/job/server/FakeServer.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job.server;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import job.Job;
+import job.JobStatus;
+import job.client.ClientCallback;
+
+/**
+ *
+ * @author john
+ */
+public class FakeServer implements JobServer {
+    
+    private Map<UUID, ClientCallback> clients;
+    
+    public FakeServer() {
+        clients = new HashMap<UUID, ClientCallback>();
+    }
+
+    @Override
+    public synchronized UUID getSession(ClientCallback client) throws RemoteException {
+        UUID id = UUID.randomUUID();
+        clients.put(id, client);
+        return id;
+    }
+
+    @Override
+    public synchronized void endSession(UUID id) throws RemoteException {
+        clients.remove(id);
+    }
+
+    @Override
+    public synchronized Job getNextJob(UUID id) throws RemoteException {
+        return new FakeJob();
+    }
+
+    @Override
+    public synchronized void returnJob(UUID id, Job job) throws RemoteException {
+        
+    }
+    
+    public static void main(String... args) throws RemoteException {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        FakeServer server = new FakeServer();
+        JobServer stub = (JobServer)UnicastRemoteObject.exportObject(server, 0);
+        Registry registry = LocateRegistry.getRegistry();
+        registry.rebind("JobServer", stub);
+    }
+}
--- PrimeFactorizationServer/src/job/server/factor/TrialDivisionServer.java
+++ PrimeFactorizationServer/src/job/server/factor/TrialDivisionServer.java
@@ -4,10 +4,17 @@
  */
 package job.server.factor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.text.NumberFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import job.server.JobServer;
 
 /**
@@ -15,13 +22,48 @@ import job.server.JobServer;
  * @author john
  */
 public class TrialDivisionServer {
-    public static void main(String... args) throws RemoteException {
+    private TrialDivisionManager manager;
+    private Thread managerThread;
+    
+    public TrialDivisionServer() {
+        manager = new TrialDivisionManager();
+        managerThread = new Thread(manager);
+        
+    }
+    
+    public void init() throws RemoteException {        
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
-        JobServer server = new TrialDivisionManager();
-        JobServer stub = (JobServer)UnicastRemoteObject.exportObject(server, 0);
+        
+        JobServer stub = (JobServer)UnicastRemoteObject.exportObject(manager, 0);
         Registry registry = LocateRegistry.getRegistry();
         registry.rebind("JobServer", stub);
+        
+        managerThread.run();
+    }
+    
+    public void menu() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        NumberFormat nf = NumberFormat.getInstance();
+        while(true) {
+            System.out.println("Please enter a positive number: ");
+            try {
+                String s = br.readLine();
+                BigInteger number = new BigInteger(s);
+                manager.setNumber(number);
+            } catch (IOException ex) {
+                Logger.getLogger(TrialDivisionServer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch(NumberFormatException ex) {
+                Logger.getLogger(TrialDivisionServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+    
+    public static void main(String... args) throws RemoteException {
+        TrialDivisionServer server = new TrialDivisionServer();
+        server.init();
+        server.menu();
     }
 }
