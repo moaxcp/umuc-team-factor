e1e4efa9dd5be3e52ee2c8ce32a1be41c0913361
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-11-14 02:32:16 +0000

    JobServer now works with RMI.

--- PrimeFactorizationServer/src/job/server/factor/TrialDivisionServer.java
+++ PrimeFactorizationServer/src/job/server/factor/TrialDivisionServer.java
@@ -4,12 +4,24 @@
  */
 package job.server.factor;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import job.server.JobServer;
+
 /**
  *
  * @author john
  */
 public class TrialDivisionServer {
-    public static void main(String... args) {
-        
+    public static void main(String... args) throws RemoteException {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        JobServer server = new TrialDivisionManager();
+        JobServer stub = (JobServer)UnicastRemoteObject.exportObject(server, 0);
+        Registry registry = LocateRegistry.getRegistry();
+        registry.rebind("JobServer", stub);
     }
 }
