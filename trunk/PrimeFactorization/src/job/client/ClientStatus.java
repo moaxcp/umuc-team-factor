2c010aa700ed52757553fa59411fe66a7b153be0
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-11-14 02:26:55 +0000

    implemented class

--- trunk/PrimeFactorization/src/job/client/ClientStatus.java
+++ trunk/PrimeFactorization/src/job/client/ClientStatus.java
@@ -4,10 +4,33 @@
  */
 package job.client;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+import job.Job;
+import job.JobStatus;
+
 /**
  *
  * @author john
  */
 public class ClientStatus {
+    private Map<UUID, JobStatus> jobStatus;
+    private UUID session;
     
+    ClientStatus(Map<UUID, Job> jobs, UUID session) {
+        this.session = session;
+        for(UUID id : jobs.keySet()) {
+            jobStatus.put(id, jobs.get(id).getStatus());
+        }
+        jobStatus = Collections.unmodifiableMap(jobStatus);
+    }
+    
+    public UUID getSessionID() {
+        return session;
+    }
+    
+    public Map<UUID, JobStatus> getJobStatus() {
+        return jobStatus;
+    }
 }
