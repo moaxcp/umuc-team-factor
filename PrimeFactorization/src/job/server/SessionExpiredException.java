18987059e3ea71236b47cef71cd74b6988a42d27
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-11-18 13:21:02 +0000

    Added SessionExpiredException to support an expired client.

---
+++ PrimeFactorization/src/job/server/SessionExpiredException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job.server;
+
+/**
+ *
+ * @author john
+ */
+public class SessionExpiredException extends Exception {
+    
+}
