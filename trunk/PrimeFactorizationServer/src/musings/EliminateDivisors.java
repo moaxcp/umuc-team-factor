631732f22936404baa8851a551f3cb788cf07eb7
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-11-26 19:56:26 +0000

    optimized pattern finding algorithm and made function for finding pattern with a prime number instead of just doing each one manually.

--- trunk/PrimeFactorizationServer/src/musings/EliminateDivisors.java
+++ trunk/PrimeFactorizationServer/src/musings/EliminateDivisors.java
@@ -4,10 +4,18 @@
  */
 package musings;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  *
@@ -15,300 +23,216 @@ import java.util.Map;
  */
 public class EliminateDivisors {
 
-    public static void printHeader(int size) {
+    public static Map<Long, Long> primeSize;
+    public static Map<Long, Long> primeStart;
+    public static SortedSet<Long> primes;
+
+    public static void printHeader(long size) {
         System.out.printf("%1$7s", "");
         for (int i = 1; i <= size; i++) {
             if (i % 10 == 0) {
-                System.out.printf("%1$5d", i);
+                System.out.printf("%1$9d", i);
             } else {
-                System.out.print("     ");
+                System.out.printf("%1$9s", "");
             }
         }
         System.out.println();
     }
 
-    public static void printLine(String msg, List<Integer> line) {
-        System.out.printf("%1$7s", msg);
-        for (Integer i : line) {
-            System.out.printf("%1$5d", i);
+    public static void printLine(Long number, List<Long> line) {
+        System.out.printf("%1$7d", number);
+        for (Long i : line) {
+            System.out.printf("%1$9d", i);
         }
         System.out.println();
     }
 
-    public static void printLargePattern(Map<List<Integer>, Integer> strings) {
-        if (strings.keySet().size() >= 1) {
-            List<Integer> large = strings.keySet().iterator().next();
-            Integer count = strings.get(large);
-            for (List<Integer> i : strings.keySet()) {
-                if(strings.get(i) > count) {
-                    large = i;
-                    count = strings.get(i);
-                }
-            }
-            int high = 0;
-            for(int i : large) {
-                if(i > high) {
+    public static void printPattern(List<Long> string) {
+        if (string != null) {
+            long high = 0;
+            long total = 0;
+            for (Long i : string) {
+                if (i.compareTo(high) > 0) {
                     high = i;
                 }
+                total += i;
             }
+            System.out.printf("%1$7s: ", "");
+            System.out.println(string);
             System.out.printf("%1$7s", "");
-            System.out.println(large);
+            System.out.println(" size: " + string.size() + " high: " + high + " total: " + total);
+        } else {
             System.out.printf("%1$7s", "");
-            System.out.println(" size: " + large.size() + " count: " + count + " high: " + high);
+            System.out.println("There is no pattern with current size.");
         }
     }
 
-    public static Map<List<Integer>, Integer> findPattern(List<Integer> list) {
-        Map<List<Integer>, Integer> patterns = new HashMap<List<Integer>, Integer>();
-        for (int i = 1; i <= list.size(); i++) {
-            List<Integer> string = new ArrayList<Integer>();
-            for (int j = 0; j < i; j++) {
-                string.add(list.get(j));
-            }
-            boolean match = true;
-            int k = 0;
-            int count = 0;
-            for (int j = 0; j < list.size(); j++) {
-                if (k == string.size()) {
-                    count++;
-                    k = 0;
-                }
-                if (!list.get(j).equals(string.get(k))) {
-                    match = false;
-                    break;
-                }
-                k++;
-            }
-            if (match) {
-                Integer oldCount = patterns.get(string);
-                oldCount = oldCount == null ? 0 : oldCount;
-                patterns.put(string, count + oldCount);
+    public static void printDivisions(int divisions, long size) {
+        BigDecimal times = new BigDecimal(size);
+        times = times.divide(BigDecimal.valueOf(divisions), MathContext.DECIMAL128);
+        System.out.printf("%1$7s%2$d divisions up to %3$d. %4$.7f times faster.\n", "", divisions, size, times);
+    }
+
+    private static boolean match(List<BigInteger> first, List<BigInteger> second) {
+        if (first.size() != second.size()) {
+            return false;
+        }
+        for (int i = 0; i < first.size(); i++) {
+            BigInteger bi = first.get(i);
+            if (!bi.equals(second.get(i))) {
+                return false;
             }
         }
-        return patterns;
+        return true;
+    }
+
+    public static List<Long> findPatternNew(List<Long> list) {
+        TreeMap<Integer, Integer> sizeIndex = new TreeMap<Integer, Integer>();
+        Map<Integer, Integer> sizeCount = new HashMap<Integer, Integer>();
+        size_loop:
+        for (int i = 1; i <= list.size(); i++) {
+            List<Integer> remove = new ArrayList<Integer>();
+            boolean removing = false;
+            for (Integer j : sizeIndex.keySet()) {
+                if (removing) {
+                    //System.out.println("removing " + list.subList(0, j));
+                    remove.add(j);
+                    continue;
+                }
+                Long stringAt = list.get(sizeIndex.get(j));
+                Long listAt = list.get(i - 1);
+                //System.out.println("Checking " + list.subList(0, j) + " stringAt[" + sizeIndex.get(j) + "]: " + stringAt + " listAt[" + (i - 1) + "]: " + listAt);
+                if (!stringAt.equals(listAt)) {
+                    //System.out.println("removing " + list.subList(0, j));
+                    remove.add(j);
+                } else {
+                    if (j - 1 == sizeIndex.get(j)) {
+                        //System.out.println("setting back to " + 0 + " " + list.subList(0, j));
+                        sizeIndex.put(j, 0);
+                        sizeCount.put(j, sizeCount.get(j) + 1);
+                        if (sizeCount.get(j) >= 2) {
+                            for (Integer x : sizeIndex.keySet()) {
+                                if (x != j) {
+                                    remove.add(j);
+                                }
+                            }
+                            break size_loop;
+                        }
+                    } else {
+                        //System.out.println("incrementing to " + (sizeIndex.get(j) + 1) + " " + list.subList(0, j));
+                        sizeIndex.put(j, sizeIndex.get(j) + 1);
+                        removing = true;
+                    }
+                }
+            }
+            for (int j : remove) {
+                sizeIndex.remove(j);
+            }
+            //System.out.println("adding: " + list.subList(0, i));
+            sizeIndex.put(i, 0);
+            sizeCount.put(i, 0);
+        }
+
+        int size = sizeIndex.firstEntry().getKey();
+        //System.out.println("size: " + size + " list.size()" + list.size() + " count: " + sizeCount.get(size));
+        if (size != list.size() && sizeCount.get(size) >= 2) {
+            return list.subList(0, size);
+        } else {
+            return null;
+        }
+    }
+
+    public static void report(long number, long size) {
+        //printHeader(size);
+        List<Long> line = new ArrayList<Long>();
+        List<Long> counts = new ArrayList<Long>();
+        int divisions = 0;
+        long counter = 0;
+
+        Iterator<Long> current = primes.iterator();
+        Iterator<Long> next = primes.iterator();
+        next.next();
+        long currentl = current.next();
+        long nextl = next.next();
+
+        while (currentl <= number) {
+            line.add(currentl);
+            counts.add(nextl - currentl);
+            divisions++;
+            currentl = current.next();
+            if (currentl <= number) {
+                nextl = next.next();
+            }
+        }
+
+        line.add(currentl);
+        divisions++;
+        currentl += 1;
+        counter += 1;
+
+
+        while (currentl <= size) {
+            boolean match = true;
+            for (long l : primes) {
+                if (l > number) {
+                    break;
+                }
+                //System.out.println(bi + " is " + currentbi.mod(bi).equals(BigInteger.ZERO) + " for " + currentbi);
+                match &= !(currentl % l == 0);
+            }
+            //System.out.println("match is " + match);
+            if (match) {
+                line.add(currentl);
+                counts.add(counter);
+                counter = 0;
+                divisions++;
+            }
+            counter += 1;
+            currentl += 1;
+        }
+        //printLine(number, line);
+        //printLine(number, counts);
+        int countSize = counts.size();
+        for (Long i : primes) {
+            if (i.compareTo(number) > 0) {
+                break;
+            }
+            counts.remove(0);
+        }
+        System.out.printf("%1$7d: Counts: %2$d Counts removed: %3$d\n", number, countSize, countSize - counts.size());
+        printPattern(findPatternNew(counts));
+        printDivisions(divisions, size);
     }
 
     public static void main(String... args) {
+        primes = new TreeSet<Long>();
+        primeSize = new TreeMap<Long, Long>();
+        primeStart = new TreeMap<Long, Long>();
 
-        int size = 50;
-        List<Integer> line = new ArrayList<Integer>();
-        List<Integer> counts = new ArrayList<Integer>();
-        int counter = 0;
+        primes.add(2l);
+        primes.add(3l);
+        primes.add(5l);
+        primes.add(7l);
+        primes.add(11l);
+        primes.add(13l);
+        primes.add(17l);
+        primes.add(19l);
+        primes.add(23l);
+        primes.add(29l);
 
-        printHeader(size);
+        primeSize.put(2l, 1000l);
+        primeSize.put(3l,  2000l);
+        primeSize.put(5l,  5000l);
+        primeSize.put(7l,  200000l);
+        primeSize.put(11l, 500000l);
+        primeSize.put(13l, 1000000l);
+        primeSize.put(17l, 2000000l);
+        primeSize.put(19l, 100000000l);
+        //primeSize.put(23l, 1000000000l);
 
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i % 2 != 0 && i % 3 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
+
+        for (Long l : primeSize.keySet()) {
+            report(l, primeSize.get(l));
         }
-        printLine("3:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
-        
-        size = 100;
-
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i == 5) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
-        }
-        printLine("5:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
-        
-        size = 1000;
-
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i == 5) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 7) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
-        }
-        printLine("7:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
-        
-        size = 5000;
-
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i == 5) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 7) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 11) {
-                line.add(i);
-                counts.add(4);
-                counter = 0;
-            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
-        }
-        printLine("11:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
-        
-        size = 70000;
-
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i == 5) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 7) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 11) {
-                line.add(i);
-                counts.add(4);
-                counter = 0;
-            } else if (i == 13) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0 && i % 13 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
-        }
-        printLine("13:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
-        
-        size = 1500000;
-
-        line.add(2);
-        for (int i = 3; i <= size; i++) {
-
-            if (i == 3) {
-                line.add(i);
-                counts.add(1);
-            } else if (i == 5) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 7) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 11) {
-                line.add(i);
-                counts.add(4);
-                counter = 0;
-            } else if (i == 13) {
-                line.add(i);
-                counts.add(2);
-                counter = 0;
-            } else if (i == 17) {
-                line.add(i);
-                counts.add(4);
-                counter = 0;
-            } else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0 && i % 13 != 0 && i % 17 != 0) {
-                line.add(i);
-                counts.add(counter);
-                counter = 0;
-            }
-            counter++;
-        }
-        printLine("17:", line);
-        line.clear();
-        printLine("", counts);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        counts.remove(0);
-        printLargePattern(findPattern(counts));
-        counts.clear();
-        counter = 0;
     }
 }
