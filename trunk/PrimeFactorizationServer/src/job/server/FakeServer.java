a00338e07b64a671c8339935c6f7f0b9f7c8f67c
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-12-02 01:26:50 +0000

    added testing for stop on fake server.

--- trunk/PrimeFactorizationServer/src/job/server/FakeServer.java
+++ trunk/PrimeFactorizationServer/src/job/server/FakeServer.java
@@ -1,55 +1,62 @@
 package job.server;
 
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import job.Job;
 import job.client.ClientCallback;
 
 /**
- * A simple JobServer that creates FakeJobs. This server is used to test the client.
+ * A simple JobServer that creates FakeJobs. This server is used to test the
+ * client.
  */
-public class FakeServer implements JobServer {
-    
+public class FakeServer extends ProcessManager {
+
     private Map<UUID, ClientCallback> clients;
-    
+    private int jobs;
+    Random rand;
+
     public FakeServer() {
         clients = new HashMap<UUID, ClientCallback>();
+        rand = new Random();
     }
 
     @Override
-    public synchronized UUID getSession(ClientCallback client) throws RemoteException {
-        UUID id = UUID.randomUUID();
-        clients.put(id, client);
-        return id;
+    public Job getNextJob(UUID id) throws RemoteException {
+        synchronized (this) {
+            jobs++;
+        }
+        return new FakeJob(rand.nextInt(20) + 10);
     }
 
     @Override
-    public synchronized void endSession(UUID id) throws RemoteException {
-        clients.remove(id);
+    public void returnJob(UUID id, Job job) throws RemoteException {
+        boolean stop = false;
+        synchronized (this) {
+            if (jobs >= 10) {
+                jobs = 0;
+                stop = true;
+            }
+        }
+        if (stop) {
+            stopJobs();
+        }
     }
 
-    @Override
-    public synchronized Job getNextJob(UUID id) throws RemoteException {
-        return new FakeJob();
-    }
-
-    @Override
-    public synchronized void returnJob(UUID id, Job job) throws RemoteException {
-        
-    }
-    
     public static void main(String... args) throws RemoteException {
         if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new SecurityManager());
+            System.setSecurityManager(new RMISecurityManager());
         }
         FakeServer server = new FakeServer();
-        JobServer stub = (JobServer)UnicastRemoteObject.exportObject(server, 0);
+        JobServer stub = (JobServer) UnicastRemoteObject.exportObject(server, 0);
         Registry registry = LocateRegistry.getRegistry();
         registry.rebind("JobServer", stub);
+        server.run();
     }
 }
