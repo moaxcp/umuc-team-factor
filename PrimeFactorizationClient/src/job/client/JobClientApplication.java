b4b2c06c85aa74eec98f1f22acd21d0781a6585a
moaxcp@gmail.com <moaxcp@gmail.com@58a3b328-129a-81e9-2f32-2c552756214b>
2013-12-04 17:28:30 +0000

    Added properties for configuration of client and server.

--- PrimeFactorizationClient/src/job/client/JobClientApplication.java
+++ PrimeFactorizationClient/src/job/client/JobClientApplication.java
@@ -1,26 +1,43 @@
 package job.client;
 
-
-import java.rmi.NotBoundException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import job.server.JobServer;
+import java.util.Properties;
 
 /**
  * Creates a JobClient and runs it.
  */
 public class JobClientApplication {
 
-    public static void main(String... args) throws RemoteException {
+    public static void main(String... args) throws RemoteException, FileNotFoundException, IOException {
+        Properties props = new Properties(JobClient.getDefaultProperties());
+        if (args.length == 1) {
+            //load from user location
+            FileInputStream fin = new FileInputStream(new File(args[0]));
+            props.load(fin);
+        } else {
+            //load from default location
+            File file = new File("client.properties");
+            if (file.exists()) {
+                FileInputStream fin = new FileInputStream(file);
+                props.load(fin);
+            } else {
+                //store defaults to default location (used as template)
+                FileOutputStream fout = new FileOutputStream(file);
+                JobClient.getDefaultProperties().store(fout, "default client.properties");
+            }
+        }
+        
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
         }
         
-        int cores = Runtime.getRuntime().availableProcessors() - 1;
-        
-        JobClient client = new JobClient(cores <= 1 ? 1 : cores);
+        JobClient client = new JobClient(props);
         client.run();
     }
 }
